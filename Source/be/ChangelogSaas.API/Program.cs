86d6bcfc6df5a62d7c577ded0c81cb102c810ea6
Sotatek-DuHoang <du.hoang@sotatek.com>
2026-06-03 10:23:11 +0700

    fix: add CORS

--- Source/be/ChangelogSaas.API/Program.cs
+++ Source/be/ChangelogSaas.API/Program.cs
@@ -39,10 +39,19 @@ builder.Services
 
 builder.Services.AddAuthorization();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Frontend", policy =>
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
+app.UseCors("Frontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
