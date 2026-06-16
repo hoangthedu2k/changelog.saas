a296f54caf458bd75044e2e6d198f28ed59350cb
Sotatek-DuHoang <du.hoang@sotatek.com>
2026-06-16 09:13:38 +0700

    fix: update domain url

--- Source/fe/src/environments/environment.ts
+++ Source/fe/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://changelogsaas-production.up.railway.app/api',
-    publicUrl: 'https://changelog-saas-one.vercel.app',
+    apiUrl: 'https://api.horachangelog.com/api',
+    publicUrl: 'https://horachangelog.com',
     oauth: {
         googleClientId: '506824940150-0abvafpolk69vp0g2cbrmrsigpenhmsp.apps.googleusercontent.com',
         facebookAppId: '1770266383943128',
